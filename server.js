5b5bfffa453847699fb7a325fc1e7309ae0f91f1
konepadith <74092214+konepadith@users.noreply.github.com>
2022-02-07 19:45:16 +0700

    code finish

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const bodyParser = require("body-parser")
 const mysql = require("mysql")
 const cors = require("cors")
 const crypto = require('crypto')
+const nodemailer = require('nodemailer')
+const { info } = require("console")
 require('dotenv').config()
 
 app.use(bodyParser.json())
@@ -31,6 +33,28 @@ const fileStorageEngine = multer.diskStorage({ //manage Storage Parameter
 // Create variable to upload image
 const upload = multer({storage :fileStorageEngine})
 
+//NodeMailler
+// const transporter = nodemailer.createTransport({
+//     service:'gmail',
+//     auth:{
+//         user:'takeme.home.lao@gmail.com',
+//         pass:'Takeme.homerdsjklutiro134533'
+//     }
+// })
+// const mailOption = {
+//     from: 'takeme.home.lao@gmail.com',
+//     to:'konepadithspydee@gmail.com',
+//     subject:'Sending Email using Nodejs',
+//     text:"Hi I'm Batman"
+// }
+// transporter.sendMail(mailOption,function(error,info){
+//     if (error) {
+//         console.log(error)
+//     } else {
+//         console.log('Email sent:'+info.response)
+//     }
+// })
+
 // connection Database
 const dbCon = mysql.createConnection({
     host: 'localhost',
@@ -38,6 +62,9 @@ const dbCon = mysql.createConnection({
     password:'',
     database:'takemehome'
 })
+
+
+
 dbCon.connect();
 
 //>>>>>>> API ZONE <<<<<<<<
@@ -50,6 +77,64 @@ app.get('/' , (req,res)=>{
 		written_by: "Pelay"
 	})
 })
+//send email
+app.post('/mailing',(req,res)=>{
+    const {emailto,subject,text}=req.body
+    const transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:'takeme.home.lao@gmail.com',
+            pass:'Takeme.homerdsjklutiro134533'
+        }
+    })
+    const mailOption = {
+        from: 'takeme.home.lao@gmail.com',
+        to:emailto,
+        subject:subject,
+        text:text
+    }
+    transporter.sendMail(mailOption,function(error,info){
+        if (error) {
+            console.log(error)
+            return res.send({error:true,status:0,msg:'fail'})
+        } else {
+            console.log('Email sent:'+info.response)
+            return res.send({error:false,data:info.response,status:1,msg:'succesfull'})
+        }
+    })
+    
+})
+
+//receive mail
+app.post('/receive',(req,res)=>{
+    const{mail_name,mail_surname,mail_email,mail_phone,mail_call,mail_topic,mail_msg}=req.body
+    // console.log(req.body)
+    // return res.send({error : false,message:"success"})
+    if (!mail_name || !mail_surname || !mail_email || !mail_phone || !mail_call || !mail_topic ||!mail_msg) {
+        return res.status(400).send({error : true,message:"there null field"})
+    } else {
+        dbCon.query('INSERT INTO tb_receivemail(receivemail_name, receivemail_surname, receivemail_email,receivemail_phone,receivemail_call,receivemail_topic,receivemail_msg) VALUES(?,?,?,?,?,?,?)',[mail_name,mail_surname,mail_email,mail_phone,mail_call,mail_topic,mail_msg],(error,results,fields)=>{
+            if(error) throw error;
+            return res.send({error:false,data:results,message:"success",status: 1})
+        })
+    }
+})
+
+//event&news add
+app.post('/add_events',(req,res)=>{
+    const{event_topic,event_date, event_start, event_end, event_place, event_direction}=req.body
+    if (!event_topic || !event_date || !event_start || !event_end || !event_place || !event_direction) {
+        return res.status(400).send({error : true,message:"there null field"})
+    } else {
+        dbCon.query('INSERT INTO tb_events(event_topic, event_date, event_start,event_end,event_place,event_direction,event_status) VALUES(?,?,?,?,?,?,?)',[event_topic,event_date, 'From '+event_start, 'To '+event_end, event_place, event_direction, 1],(error,results,fields)=>{
+            if(error) throw error;
+            return res.send({error:false,data:results,message:"success",status: 1})
+        })
+    }
+})
+
+//represent Events
+app.get('/events_data')
 
 // donate gateway
 app.post('/donate',async (req,res,next)=>{
@@ -68,31 +153,14 @@ app.post('/donate',async (req,res,next)=>{
             'customer': customer.id,
           });
         console.log('charge-->',charge)
-        return res.send({ error : false , data: charge, message:'Succesfully' });
+        return res.send({ error : false , data: charge, message:'Succesfully',status:1 });
     } catch (error) {
         console.log(error)
+        return res.send({ error : true , message:'Fail',status:0 });
     }
 
 next()
 });
-
-// Represent * product
-// app.get('/dogs_info', (req,res)=>{
-//     dbCon.query('SELECT * FROM tb_dogs',(error, results,fields)=>{
-//         if(error) throw error;
-//         let message = ""
-// 		let status 
-// 		if(results === undefined || results.lenght == 0){
-// 			message ="Book table is empty"
-// 			status=0
-// 		}else {
-// 			message ="Succesfully retrieved all books"
-// 			status=1
-// 		}
-// 		return res.send({ error : false , data: results, message:message, status:status });
-//     })
-// })
-
 // add dog Data
 app.post("/add_dog_data", upload.single('image'),(req,res)=>{
 
@@ -109,7 +177,29 @@ app.post("/add_dog_data", upload.single('image'),(req,res)=>{
     }
 })
 
-// add user data
+// add dog Data array
+app.post("/add_dog_data_array", upload.array('images',5),(req,res)=>{
+
+    const {dog_name,dog_dob,dog_gender,dog_species} = req.body
+    let dog_img=req.files[0].filename
+    let dog_img2=req.files[1].filename
+    let dog_img3=req.files[2].filename
+    let dog_img4=req.files[3].filename
+    let dog_img5=req.files[4].filename
+    console.log(req.files[1].filename)
+    // res.send("multi appry")
+    // console.log(req.body,req.file.filename)
+    if (!dog_name || !dog_dob || !dog_gender || !dog_species || !dog_img) {
+        return res.status(400).send({error : true,message:"there null field"})
+    } else {
+        dbCon.query('INSERT INTO tb_dogs (dog_name, dog_dob, dog_gender, dog_species, dog_img, dog_img2,dog_img3,dog_img4,dog_img5) VALUES(?,?,?,?,?,?,?,?,?)',[dog_name,dog_dob,dog_gender,dog_species,"http://localhost:3000/present/"+dog_img,"http://localhost:3000/present/"+dog_img2,"http://localhost:3000/present/"+dog_img3,"http://localhost:3000/present/"+dog_img4,"http://localhost:3000/present/"+dog_img5],(error, results,fields)=>{
+            if(error) throw error;
+            return res.send({error:false,data:results,message:"success"})
+        })
+    }
+})
+
+// register
 app.post("/add_user_data", upload.single('image'),(req,res)=>{
 
     const{user_name,user_surname,user_gender,user_password,user_email,user_dob,user_village,user_district,user_province,user_workplace,user_phoneNumber}=req.body
@@ -151,7 +241,7 @@ app.post('/login',  (req,res)=>{
         
         return res.send({status: 0 })
     }else{
-        dbCon.query("SELECT * FROM tb_users WHERE user_email = ? AND user_password = ?",[user_email,crypto.createHash('md5').update(user_password).digest('hex')],async(error,results,fields)=>{
+        dbCon.query("SELECT * FROM tb_users WHERE user_email = ? AND user_password = ?",[user_email,crypto.createHash('md5').update(user_password).digest('hex')],(error,results,fields)=>{
             if(error) throw error;
             let message = ""
             if (results.length > 0) {
@@ -167,9 +257,29 @@ app.post('/login',  (req,res)=>{
     }
 })
 
+//requireddorm
+app.post('/required',  (req,res)=>{
+    const {user_id,dog_id,q_1,q_2,q_3,q_4,q_5,q_6,q_7,q_8,q_9,q_10,q_11,q_12,q_13,q_14,q_15,q_16,q_17,q_18,q_19} =req.body
+    console.log(req.body)
+
+
+
+
+    if (!user_id|| !dog_id || !q_1|| !q_3|| !q_5|| !q_7|| !q_8|| !q_9|| !q_10|| !q_11|| !q_12|| !q_13|| !q_14|| !q_15|| !q_16|| !q_17|| !q_18|| !q_19) {
+        return res.send({status: 0 })
+    }else{
+        dbCon.query("INSERT INTO tb_form_adopt (user_id,dog_id,q_1,q_2,q_3,q_4,q_5,q_6,q_7,q_8,q_9,q_10,q_11,q_12,q_13,q_14,q_15,q_16,q_17,q_18,q_19) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",[user_id,dog_id,q_1,q_2,q_3,q_4,q_5,q_6,q_7,q_8,q_9,q_10,q_11,q_12,q_13,q_14,q_15,q_16,q_17,q_18,q_19],(error,results,fields)=>{
+            if(error) throw error;
+            let message = ""
+            message = "sign up successful"
+            return res.send({error:false,data:results,message:"success",status: 1})
+        })
+    }
+})
+
 // represent Dogs data
 app.get('/dogs_data',(req,res)=>{
-    dbCon.query('SELECT * ,timestampdiff(year,dog_dob,CURRENT_DATE) as age FROM tb_dogs',(error, results,fields)=>{
+    dbCon.query('SELECT * ,timestampdiff(year,dog_dob,CURRENT_DATE) as age FROM tb_dogs where dog_status = 0',(error, results,fields)=>{
         if(error) throw error;
         let message = ""
 		let status 
